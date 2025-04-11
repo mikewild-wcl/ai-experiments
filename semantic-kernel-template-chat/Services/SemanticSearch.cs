047e7507b1d3f925bff7ad4d4551b5413d61d8a5
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2025-04-11 17:42:23 +0100

    Fix filter to include empty filename

--- semantic-kernel-template-chat/Services/SemanticSearch.cs
+++ semantic-kernel-template-chat/Services/SemanticSearch.cs
@@ -12,10 +12,15 @@ public class SemanticSearch(
         var queryEmbedding = await embeddingGenerator.GenerateEmbeddingVectorAsync(text);
         var vectorCollection = vectorStore.GetCollection<string, SemanticSearchRecord>("data-semantic-kernel-template-chat-ingested");
 
+        /*
+        var oldFilter = filenameFilter is { Length: > 0 }
+            ? new VectorSearchFilter().EqualTo(nameof(SemanticSearchRecord.FileName), filenameFilter)
+            : null;
+        */
         var nearest = await vectorCollection.VectorizedSearchAsync(queryEmbedding, new VectorSearchOptions<SemanticSearchRecord>
         {
             Top = maxResults,
-            Filter = x => string.Equals(x.FileName, filenameFilter)
+            Filter = x => string.IsNullOrEmpty(filenameFilter) || string.Equals(x.FileName, filenameFilter)
         });
 
         var results = new List<SemanticSearchRecord>();
