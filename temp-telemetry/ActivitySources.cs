70746462e4a5c545a1978b798720a3882161e7de
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2025-11-24 11:03:45 +0000

    Update nugets, remove temp-telemetry project

--- temp-telemetry/ActivitySources.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Diagnostics;
-
-namespace temp_telemetry;
-
-internal static class ActivitySources
-{
-    public static readonly ActivitySource MyActivitySource = new(
-                "MyCompany.MyProduct.MyLibrary");
-}
